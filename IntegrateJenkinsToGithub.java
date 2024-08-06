d85529a4cf7ef09f02c304a4fd88b1a617d240f7
devloper2 <devloper2@devops.com>
2024-08-06 04:12:59 -0400

    secondCommit

--- IntegrateJenkinsToGithub.java
+++ IntegrateJenkinsToGithub.java
@@ -2,5 +2,6 @@ class IntegrateJenkinsToGithub {
     public static void main(String[] args) {
         System.out.println("Integrate Jenkins to Github using webhook!");
 	System.out.println("Adding another line by devloper");
+	System.out.println("Adding another second line by devloper");
     }
 }
