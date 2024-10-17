255ca6d5b7e84fe08b482fe8b283dbdeb07b1755
Gaurav1517 <145908403+Gaurav1517@users.noreply.github.com>
2024-10-17 14:26:22 +0530

    Delete IntegrateJenkinsToGithub.java

--- IntegrateJenkinsToGithub.java
+++
@@ -1,8 +0,0 @@
-class IntegrateJenkinsToGithub {
-    public static void main(String[] args) {
-        System.out.println("Integrate Jenkins to Github using webhook!");
-	System.out.println("Adding another line by devloper");
-	System.out.println("Adding another second line by devloper");
-	System.out.println("Adding another third line by devloper");
-    }
-}
