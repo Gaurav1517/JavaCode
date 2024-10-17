d41019132851a452e75d7c92d94ed373ff614a8f
Gaurav1517 <145908403+Gaurav1517@users.noreply.github.com>
2024-10-17 14:32:49 +0530

    Add files via upload

---
+++ maven-java-app/src/main/java/app/example/maven_java_app/MavenJavaAppApplication.java
@@ -0,0 +1,13 @@
+package app.example.maven_java_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenJavaAppApplication.class, args);
+	}
+
+}
